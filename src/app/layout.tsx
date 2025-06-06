575c31ff4b1773aab136d776e13cf850bde4fe9c
lucasbertolo <lucasbertolo2@gmail.com>
2025-06-06 16:46:00 -0300

    feature: adding analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { ThemeRegistry } from "@/config/theme";
 import { champagneFont, mangolaineFont } from "@/config/theme/fonts";
 import type { Metadata } from "next";
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Idalgo Cortijo Advocacia",
@@ -16,7 +17,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
+      <head>
+        <Script
+          id="gtm-head"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-528JN7VG');
+            `,
+          }}
+        />
+      </head>
       <body className={`${champagneFont.variable} ${mangolaineFont.variable}`}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-528JN7VG"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <ThemeRegistry>{children}</ThemeRegistry>
       </body>
     </html>
